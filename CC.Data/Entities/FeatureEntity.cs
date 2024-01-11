0e64ff74887f11170a789e6b5defdabd61449ee9
Isaiah Schmidt <isiahdschmidt@outlook.com>
2024-01-10 21:27:17 -0500

    feature entity done

---
+++ CC.Data/Entities/FeatureEntity.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CC.Data.Entities
+{
+    public class FeatureEntity
+    {
+        [Key]
+        public int Id {get; set;}
+
+        [Required]
+        [MinLength(1, ErrorMessage = "Name must be at least 1 character")] 
+        [MaxLength(250, ErrorMessage ="Name must be no more than 250 characters")]
+        public string Name {get; set;} = string.Empty;
+
+        [Required]
+        [MinLength(1, ErrorMessage = "Description must be at least 1 character")] 
+        [MaxLength(1000, ErrorMessage ="Description must be no more than 1000 characters")]
+        public string Description {get; set;} = string.Empty;
+    }
+}
