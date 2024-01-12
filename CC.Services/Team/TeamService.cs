b9414ce23e8e66b656dea5dd5f89d3b2560884d8
Isaiah Schmidt <isiahdschmidt@outlook.com>
2024-01-11 19:46:32 -0500

    GetTeams methods done

--- CC.Services/Team/TeamService.cs
+++ CC.Services/Team/TeamService.cs
@@ -1,7 +1,9 @@
 using CC.Data;
 using CC.Data.Entities;
 using CC.Models.Team;
+using CC.Models.User;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
 namespace CC.Services.Team
 {
@@ -23,7 +25,6 @@ namespace CC.Services.Team
             {
                 throw new Exception("Attempted to create team with out Id Claim");
             }
-
             _context = context;            
         }
 
@@ -31,13 +32,43 @@ namespace CC.Services.Team
         {
             TeamEntity entity = new()
             {
-                Name = model.Name
+                Name = model.Name,
+                OwnerId = _userId
             };
 
             _context.Teams.Add(entity);
             var changes = await _context.SaveChangesAsync();
             if(changes != 1) return false;
                 return true;
+        }
+
+        public async Task<TeamDetail?> GetTeamByIdAsync(int teamId)
+        {
+            TeamEntity? entity = await _context.Teams.FindAsync(teamId);
+            if (entity is null)
+            {
+                return null;
+            }
+            if (entity.OwnerId != _userId)
+            {
+                return null;
+            }
+            TeamDetail detail = new()
+            {
+                Id = entity.Id,
+                Name = entity.Name
+            };
+            return detail;
+        }
+
+        public async Task<IEnumerable<TeamDetail>> GetAllTeamsAsync()
+        {
+            List<TeamDetail> teams = await _context.Teams.Where(entity => entity.OwnerId == _userId)
+            .Select(entity => new TeamDetail{
+                Id = entity.Id,
+                Name = entity.Name 
+            }).ToListAsync();
+            return teams;
         }
     }
 }
