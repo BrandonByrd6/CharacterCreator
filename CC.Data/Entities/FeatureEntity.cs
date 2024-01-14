03a8c20c1d6428a411a6cb4b060c8d4ee44d5116
BrandonByrd6 <145848997+BrandonByrd6@users.noreply.github.com>
2024-01-14 15:52:04 -0500

    Revert "Brandon byrd6/add character to team"

--- CC.Data/Entities/FeatureEntity.cs
+++ CC.Data/Entities/FeatureEntity.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -21,10 +20,5 @@ namespace CC.Data.Entities
         [MinLength(1, ErrorMessage = "Description must be at least 1 character")] 
         [MaxLength(1000, ErrorMessage ="Description must be no more than 1000 characters")]
         public string Description {get; set;} = string.Empty;
-
-        [Required]
-        [ForeignKey(nameof(Owner))]
-        public int OwnerId { get; set; }
-        UserEntity Owner { get; set; } = null!;
     }
 }
--- CC.Services/Character/ICharacterService.cs
+++ CC.Services/Character/ICharacterService.cs
@@ -5,15 +5,4 @@ public interface ICharacterService
 {
     Task<bool> CreateCharacterAsync(CreateCharacter request);
 
-    Task<IEnumerable<CharacterDetail>> getAllCharactersAsync();
-    
-    Task<CharacterDetail?> getCharacterById(int characterId);
-
-    Task<bool> UpdateCharacterAsync(CharacterUpdate request);
-
-    Task<bool> DeleteCharacterAsync(int id);
-
-    Task<bool> AddFeatureToCharacterAsync(CharacterFeatureAdd request);
-
-    Task<bool> AddCharacterToTeamAsync(CharacterTeamAdd request); 
 }
